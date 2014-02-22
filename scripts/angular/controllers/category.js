2a973957057b9eabb32164bff9700c08640b78e7
calidion <calidion@gmail.com>
2014-02-22 14:07:07 +0800

    fixed cateory list bugs
    fixed cateory list bugs

--- scripts/angular/controllers/category.js
+++ scripts/angular/controllers/category.js
@@ -12,10 +12,12 @@ define(['util', 'skyex'], function(util, skyex) {
           id: pid
       };
       
-
       console.log(pids);
       if (cacheCategory[pid]) {
-        if (((pid == 0) && (pids.length <= 0)) || pid != 0) {
+        
+        if (pid == 0) {
+          pids = [0];
+        } else {
           pids.push(pid);
         }
         return cacheCategory[pid];
@@ -23,7 +25,9 @@ define(['util', 'skyex'], function(util, skyex) {
       
       return skyex.post($http, params, function(response) {
         cacheCategory[pid] = response;
-        if (((pid == 0) && (pids.length <= 0)) || pid != 0) {
+        if (pid == 0) {
+          pids = [0];
+        } else {
           pids.push(pid);
         }
         return response;
@@ -31,57 +35,57 @@ define(['util', 'skyex'], function(util, skyex) {
     }
   };
   
-  category.controller = ['$scope', '$rootScope', '$location', 'categories', function($scope, $rootScope, $location, categories) {
-    console.log('inside category ctrl');
-    console.log(categories.data);
-    console.log(pids);
-    util.swap(1);
-    var title = '书籍分类';
-    var header = {
-      title: title
-    };
-    if (pids.length > 1) {
-      //
-      var pid = pids[pids.length - 1];
-      console.log(pid);
-      console.log(cacheCategory);
-      if (pid != 0) {
-        title = cacheCategory[pid].name;
-        header = {
-            title: title,
-            showBackButton: true,
-            backButtonIcon: 'arrow-l',
-            backButtonText: '返回',
+  category.controller = ['$scope', '$rootScope', '$location', 'categories',
+      function($scope, $rootScope, $location, categories) {
+        console.log('inside category ctrl');
+        console.log(categories.data);
+        console.log(pids);
+        util.swap(1);
+        var title = '书籍分类';
+        var header = {
+          title: title
         };
-      }
-      
-    }
-    
-    $rootScope.back = function() {
-      console.log('inside back');
-      var ppid = 0;
-      if (pids.length > 1) {
-        ppid = pids[pids.length - 2];
-        console.log(pids);
-        pids.pop();
         if (pids.length > 1) {
-          pids.pop();
+          //
+          var pid = pids[pids.length - 1];
+          console.log(pid);
+          console.log(cacheCategory);
+          if (pid != 0) {
+            title = cacheCategory[pid].name;
+            header = {
+                title: title,
+                showBackButton: true,
+                backButtonIcon: 'arrow-l',
+                backButtonText: '返回',
+            };
+          }
+          
         }
-        console.log(pids);
-      }
-      console.log(ppid);
-      
-      var path = '/category/' + ppid;
-      console.log(path);
-      
-
-      $location.path(path);
-    };
-    
-    $rootScope.header = header;
-    
-    $scope.categories = categories.data;
-    $scope.$on('$routeChangeSuccess', util.contentLoad);
-  }];
+        
+        $rootScope.back = function() {
+          console.log('inside back');
+          var ppid = 0;
+          if (pids.length > 1) {
+            ppid = pids[pids.length - 2];
+            console.log(pids);
+            pids.pop();
+            if (pids.length > 1) {
+              pids.pop();
+            }
+            console.log(pids);
+          }
+          console.log(ppid);
+          
+          var path = '/category/' + ppid;
+          console.log(path);
+          
+          $location.path(path);
+        };
+        
+        $rootScope.header = header;
+        
+        $scope.categories = categories.data;
+        $scope.$on('$routeChangeSuccess', util.contentLoad);
+      }];
   return category;
 });
